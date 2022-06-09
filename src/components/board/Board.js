b9047f4f9a9795a3b016d178c7c2ea07946b77d5
Viktor Denikin <leshden@yandex.ru>
2022-06-09 22:33:27 +0300

    move to game

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -2,55 +2,15 @@ import './Board.css';
 
 import {useState} from 'react';
 import Square from '../square/Square';
-import Status from '../status/Status';
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-const Board = () => {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXNext] = useState(true);
-
-  const handleClick = (i) => {
-      const squaresCopy = squares.slice();
-      if (calculateWinner(squaresCopy) || squaresCopy[i]) {
-          return;
-      }
-      squaresCopy[i] = getXorO();
-      setSquares(squaresCopy);
-      setXNext(!xIsNext);
-      console.log(`onClick cell number : ${i} value : ${squares[i]} `);
-  }
+const Board = (props) => {
 
   const renderSquare = (i) => {
-    return <Square value = {squares[i]} onClickCell={()=>(handleClick(i))} />;
-  }
-
-  function getXorO() {
-    return xIsNext ? 'X' : 'O';
+    return <Square value = {props.squares[i]} onClickCell={()=>(props.handleClick(i))} />;
   }
 
   return (
       <div>
-        <Status winner={calculateWinner(squares)} player={getXorO()} />
         <div className="board-row">
           {renderSquare(0)}
           {renderSquare(1)}
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,14 +1,60 @@
 import './Game.css';
+import {useState} from 'react';
 import Board from '../board/Board';
+import Status from '../status/Status';
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 const Game = () => {
+
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+  const [xIsNext, setXNext] = useState(true);
+  const current = history[history.length - 1];
+
+  const handleClick = (i) => {
+    const current = history[history.length - 1];
+      const squaresCopy = current.squares.slice();
+      if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+          return;
+      }
+      squaresCopy[i] = getXorO();
+
+      setHistory(history.concat([{ squares: squaresCopy}]));
+      setXNext(!xIsNext);
+
+      console.log(`onClick cell number : ${i} value : ${squaresCopy[i]} `);
+  }
+
+  function getXorO() {
+    return xIsNext ? 'X' : 'O';
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} handleClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <Status winner={calculateWinner(current.squares)} player={getXorO()} />
         <ol>{/* TODO */}</ol>
       </div>
     </div>
